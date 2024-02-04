ed9eb30964e0b03256bbb45d3f26901150dd3072
CrioUser <crio-user@criodo.com>
2024-02-04 14:07:57 +0530

    update products jsx to show cart while logged in

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,7 @@ import {
   Grid,
   InputAdornment,
   TextField,
+  Stack,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
@@ -13,12 +14,13 @@ import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
 import ProductCard from "./ProductCard";
+import Cart from "./Cart";
 import "./Products.css";
 
 // Definition of Data Structures used
 /**
  * @typedef {Object} CartItem -  - Data on product added to cart
- * 
+ *
  * @property {string} name - The name or title of the product in cart
  * @property {string} qty - The quantity of product added to cart
  * @property {string} category - The category that the product belongs to
@@ -35,6 +37,8 @@ const Products = () => {
   const [products, setProducts] = useState([]);
   const [searchBox, setSearchBox] = useState("");
   const [debounceTimeout, setDebounceTimeout] = useState(null);
+  const username = window.localStorage.getItem("username");
+
   // TODO: CRIO_TASK_MODULE_PRODUCTS - Fetch products data and store it
   /**
    * Make API call to get the products list and store it to display the products
@@ -84,10 +88,10 @@ const Products = () => {
         });
     } catch (error) {
       if (error.response)
-          enqueueSnackbar(error.response.data.message, {
-            variant: "error",
-            duration: 6000,
-          });
+        enqueueSnackbar(error.response.data.message, {
+          variant: "error",
+          duration: 6000,
+        });
       else
         enqueueSnackbar("Server is not reachable.", {
           variant: "error",
@@ -113,7 +117,9 @@ const Products = () => {
    */
   const performSearch = async (text) => {
     try {
-      const res = await axios.get(config.endpoint + "/products/search?value=" + text);
+      const res = await axios.get(
+        config.endpoint + "/products/search?value=" + text
+      );
       if (res.status === 200) {
         return res.data;
       } else
@@ -122,13 +128,12 @@ const Products = () => {
           duration: 6000,
         });
     } catch (error) {
-      if (error.response.status === 404)
-        setProducts([]);
+      if (error.response.status === 404) setProducts([]);
       else if (error.response)
-          enqueueSnackbar(error.response.data.message, {
-            variant: "error",
-            duration: 6000,
-          });
+        enqueueSnackbar(error.response.data.message, {
+          variant: "error",
+          duration: 6000,
+        });
       else
         enqueueSnackbar("Server is not reachable.", {
           variant: "error",
@@ -151,18 +156,18 @@ const Products = () => {
    *
    */
   const debounceSearch = (event, debounceTimeout) => {
-    if(debounceTimeout){
+    if (debounceTimeout) {
       clearTimeout(debounceTimeout);
     }
-    const timer = setTimeout(()=>{
-      performSearch(event.target.value).then(data => setProducts(data));
+    const timer = setTimeout(() => {
+      performSearch(event.target.value).then((data) => setProducts(data));
     }, 500);
     setDebounceTimeout(timer);
   };
 
   useEffect(() => {
-    if(isBusyLoading)
-      performAPICall().then(data => {
+    if (isBusyLoading)
+      performAPICall().then((data) => {
         setIsBusyLoading(false);
         setProducts(data);
       });
@@ -216,7 +221,6 @@ const Products = () => {
     }
   };
 
-
   // TODO: CRIO_TASK_MODULE_CART - Return if a product already exists in the cart
   /**
    * Return if a product already is present in the cart
@@ -230,8 +234,7 @@ const Products = () => {
    *    Whether a product of given "productId" exists in the "items" array
    *
    */
-  const isItemInCart = (items, productId) => {
-  };
+  const isItemInCart = (items, productId) => {};
 
   /**
    * Perform the API call to add or update items in the user's cart and update local cart data to display the latest cart
@@ -276,9 +279,7 @@ const Products = () => {
     productId,
     qty,
     options = { preventDuplicate: false }
-  ) => {
-  };
-
+  ) => {};
 
   return (
     <div>
@@ -292,16 +293,21 @@ const Products = () => {
             endAdornment: (
               <InputAdornment
                 position="end"
-                onClick={() => performSearch(searchBox).then(data => setProducts(data))}>
-                  <Search color="primary" />
+                onClick={() =>
+                  performSearch(searchBox).then((data) => setProducts(data))
+                }
+              >
+                <Search color="primary" />
               </InputAdornment>
             ),
           }}
           placeholder="Search for items/categories"
           name="search"
           value={searchBox}
-          onKeyPress={e => {if(e.key==="Enter") performSearch(searchBox)}}
-          onChange={e => {
+          onKeyPress={(e) => {
+            if (e.key === "Enter") performSearch(searchBox);
+          }}
+          onChange={(e) => {
             setSearchBox(e.target.value);
             debounceSearch(e, debounceTimeout);
           }}
@@ -317,52 +323,62 @@ const Products = () => {
           endAdornment: (
             <InputAdornment
               position="end"
-              onClick={() => performSearch(searchBox).then(data => setProducts(data))}>
-                <Search color="primary" />
+              onClick={() =>
+                performSearch(searchBox).then((data) => setProducts(data))
+              }
+            >
+              <Search color="primary" />
             </InputAdornment>
           ),
         }}
         placeholder="Search for items/categories"
         name="search"
-        sx={{ marginBottom: '2px' }}
+        sx={{ marginBottom: "2px" }}
         value={searchBox}
-        onKeyPress={e => {if(e.key==="Enter") performSearch(searchBox)}}
-        onChange={e => {
+        onKeyPress={(e) => {
+          if (e.key === "Enter") performSearch(searchBox);
+        }}
+        onChange={(e) => {
           setSearchBox(e.target.value);
           debounceSearch(e, debounceTimeout);
         }}
       />
-      <Grid container>
-        <Grid item className="product-grid">
-          <Box className="hero">
-            <p className="hero-heading">
-              India’s <span className="hero-highlight">FASTEST DELIVERY</span>{" "}
-              to your door step
-            </p>
-          </Box>
-        </Grid>
-        {isBusyLoading ? 
-          <Box className="loading">
-            <CircularProgress />
-            <p>Loading Products...</p>
-          </Box>
-        :
-        products.length > 0 ? 
-          <Grid container spacing={2} marginY={1} paddingX={1}>
-            {products.map((product) => (
-              <Grid item xs={6} md={3} key={product._id}>
-                <ProductCard product={product} />
-              </Grid>
-            ))}
+      <Stack direction={{ md: "row" }}>
+        <Grid container>
+          <Grid item className="product-grid">
+            <Box className="hero">
+              <p className="hero-heading">
+                India’s <span className="hero-highlight">FASTEST DELIVERY</span>{" "}
+                to your door step
+              </p>
+            </Box>
           </Grid>
-        :
-          <Box className="loading">
-            <SentimentDissatisfied />
-            <p>No products found</p>
-          </Box>
-        }
-      </Grid>
+          {isBusyLoading ? (
+            <Box className="loading">
+              <CircularProgress />
+              <p>Loading Products...</p>
+            </Box>
+          ) : products.length > 0 ? (
+            <Grid container spacing={2} marginY={1} paddingX={1}>
+              {products.map((product) => (
+                <Grid item xs={6} md={3} key={product._id}>
+                  <ProductCard product={product} />
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <Box className="loading">
+              <SentimentDissatisfied />
+              <p>No products found</p>
+            </Box>
+          )}
+        </Grid>
         {/* TODO: CRIO_TASK_MODULE_CART - Display the Cart component */}
+        { username ? 
+          <Box sx={{ backgroundColor: "#E9F5E1", width: { xs: "100%", md: 1/4 } }}>
+            <Cart /> 
+          </Box> : '' }
+      </Stack>
       <Footer />
     </div>
   );
