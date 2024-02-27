b1f997451b0b3f28277bab2c13519a3537b7e822
Archisman Tripathi <36406969+archismantripathi@users.noreply.github.com>
2024-02-27 20:17:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout.js";
 import Thanks from "./components/Thanks.js";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://${ipConfig.workspaceIp}/api/v1`,
 };
 
 function App() {
